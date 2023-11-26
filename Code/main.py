65ffe50a18894c9ddf9b373189b6aaa4f84b7072
C-P22 <122741482+C-P22@users.noreply.github.com>
2023-11-26 09:10:35 +0100

    generated-maze_complett
    moving charakter in a maze
    tab new maze
    c = cheat code
    g = stop moving
    colliding with walls impossible

--- Code/main.py
+++ Code/main.py
@@ -3,7 +3,7 @@ from sprite import *
 from config import *
 import sys
 import turtle
-
+from labrinth_generator import Maze
 
 class Game:
     def __init__(self):
@@ -11,6 +11,8 @@ class Game:
         self.screen = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
         self.clock = pygame.time.Clock() #framerate
         self.running = True 
+        #self.overlay = pygame.Surface((WIN_WIDTH, WIN_HEIGHT), pygame.SRCALPHA)
+        #self.overlay.fill((0, 0, 0, 128))    
 
     def new(self):
         # when a new game starts 
@@ -24,16 +26,49 @@ class Game:
     def update(self):
         self.all_sprites.update()
     def create_level(self):
-        for i,row in enumerate(level_1):
+        level = Maze(20,20,10,1)
+        for i,row in enumerate(level.maze):
             for j,colum in enumerate(row):
                 if colum =="X":
-                    Block(self,j,i)
+                    Block(self,j,i,True)
                 if colum == "P":
-                    Player(self,j,i)
+                    self.player = Player(self,j,i)
+                    Floor(self,j,i)
+                if colum == ".":
+                    x = random.randint(0,10)
+                    if x == 0:
+
+                        Block(self,j,i,False)
+                    else:
+                        Floor(self,j,i)
+    
     def draw(self):
+
         self.screen.fill(BLACK)
+        ''''''''''
         self.all_sprites.draw(self.screen)
+        
+        camera_offset = [0,0]
+        # Update camera offset to center on the player
+        camera_offset[0] = (WIN_WIDTH // 2) - self.player.rect.x
+        camera_offset[1] = (WIN_HEIGHT // 2) - self.player.rect.y
+
+        # Draw sprites at adjusted positions based on camera offset
+        for sprite in self.all_sprites:
+            sprite.rect.x += camera_offset[0]
+            sprite.rect.y += camera_offset[1]
+        '''
+        self.all_sprites.draw(self.screen)
+
+        # Reset sprite positions after drawing
+        #for sprite in self.all_sprites:
+         #   sprite.rect.x -= camera_offset[0]
+          #  sprite.rect.y -= camera_offset[1]
+
         self.clock.tick(FPS)
+        pygame.display.update()
+        self.clock.tick(FPS)
+        #self.screen.blit(self.overlay, (0, 0))
         pygame.display.update()
     def events(self):
         for event in pygame.event.get():# all events that are registrated are here beeing checkt
@@ -41,7 +76,9 @@ class Game:
             if event.type == pygame.QUIT:
                 self.playing = False
                 self.running = False
-
+        key = pygame.key.get_pressed()#checks which keys are being pressed
+        if key[pygame.K_SPACE]:
+            self.new()
     def main(self):
         while self.playing == True:
             self.events()
@@ -50,6 +87,7 @@ class Game:
         self.running = False
     def game_over(self):
         pass
+
 g = Game()
 g.new()
 while g.running:
