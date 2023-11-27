cc244840d1c5c0a45e3d43f5deb4181ebabb531e
ExpiredMilk <tamas.dimov@gmail.com>
2023-11-27 15:16:21 +0100

    sound stuff

---
+++ Code/sound_handler.py
@@ -0,0 +1,11 @@
+import pygame
+
+class SoundHandler():
+    root_folder = "sounds"
+    music_folder = root_folder + "\\" + "music"
+    level_music_file = music_folder + "\\" + "Pharao's Tomb Soundtrack final.mp3"
+
+    def __init__(self):
+        pass
+
+    def play_level_soundtrack():
