5393a7ffdff3da264d1405e9c40261b9a52c937b
C-P22 <122741482+C-P22@users.noreply.github.com>
2023-11-28 12:05:29 +0100

    wepon_yellow_block

--- Code/main.py
+++ Code/main.py
@@ -4,7 +4,7 @@ from config import *
 import sys
 import turtle
 from labrinth_generator import Maze
-
+from icecream import ic 
 
 class Game:
     def __init__(self):
@@ -13,8 +13,8 @@ class Game:
         self.clock = pygame.time.Clock() #framerate
         self.running = True 
         self.x = 0
-        self.labrinth_lenght = 10
-        self.labrinth_wigth = 10
+        self.labrinth_lenght = 8
+        self.labrinth_wigth = 8
         #self.overlay = pygame.Surface((WIN_WIDTH, WIN_HEIGHT), pygame.SRCALPHA)
         #self.overlay.fill((0, 0, 0, 128))    
 
@@ -23,11 +23,13 @@ class Game:
         self.playing = True
         self.all_sprites = pygame.sprite.LayeredUpdates() # hier können wir unsere Sprites reintun
         self.blocks= pygame.sprite.LayeredUpdates()
+        self.waende = pygame.sprite.LayeredUpdates()
         self.portal= pygame.sprite.LayeredUpdates()
         self.enemies= pygame.sprite.LayeredUpdates()
         self.attacks= pygame.sprite.LayeredUpdates()
         self.player = pygame.sprite.LayeredUpdates()
         self.powerup =  pygame.sprite.LayeredUpdates()
+        self.weapons =  pygame.sprite.LayeredUpdates()
         ##self.player = Player(self,1,2)
         self.create_level(self.labrinth_lenght,self.labrinth_wigth) 
     def update(self):
@@ -35,6 +37,7 @@ class Game:
             self.labrinth_lenght += 3
             self.labrinth_wigth += 3
             self.new()
+        #self.weapons.update()
     def create_level(self,lenght,wigth):
         level = Maze(lenght,wigth,lenght//2,wigth//2)
         for i,row in enumerate(level.maze):
@@ -43,7 +46,7 @@ class Game:
                     Wall(self,j,i,True)
                 if colum == "P":
                     self.player = Player(self,j,i)
-                    Floor(self,j,i)
+                    Floor(self,j,i) 
                 if colum == ".":
                     x = random.randint(0,50)
                     if x < 10:
@@ -55,20 +58,17 @@ class Game:
                         Floor(self,j,i)
                 if colum == "E":
                     Floor(self,j,i)
-                    Portal(self,j,i)
-    
+                    Portal(self,j,i) 
     def draw(self):
-
         self.screen.fill(BLACK)
         ''''''''''
         self.all_sprites.draw(self.screen)
-        '''
-        
+        '''  
         camera_offset = [0,0]
         # Update camera offset to center on the player
         camera_offset[0] = (WIN_WIDTH // 2) - self.player.rect.x
         camera_offset[1] = (WIN_HEIGHT // 2) - self.player.rect.y
-
+        #ic(camera_offset[0],camera_offset[1])
         # Draw sprites at adjusted positions based on camera offset
         for sprite in self.all_sprites:
             sprite.rect.x += camera_offset[0]
@@ -77,12 +77,10 @@ class Game:
         self.all_sprites.draw(self.screen)
 
         # Reset sprite positions after drawing
-        for sprite in self.all_sprites:
-            sprite.rect.x -= camera_offset[0]
-            sprite.rect.y -= camera_offset[1]
-        
+        #for sprite in self.all_sprites:
+        #    sprite.rect.x -= camera_offset[0]
+        #    sprite.rect.y -= camera_offset[1]   
         #ligth = Light(True,300)
-   
        # x = random.randint(0,9)
         #self.x = x 
         #image = pygame.image.load(f"img/light/{self.x}.png")
@@ -93,33 +91,26 @@ class Game:
         self.clock.tick(FPS)
         # self.screen.blit(self.overlay, (0, 0))
         pygame.display.update()
-
     def events(self):
         for event in pygame.event.get():  # all events that are registered are here being checkt
-
             if event.type == pygame.QUIT:
                 self.playing = False
                 self.running = False
         key = pygame.key.get_pressed()  # checks which keys are being pressed
         if key[pygame.K_SPACE]:
             self.new()
-
     def main(self):
         while self.playing == True:
             self.events()
             self.update()
             self.draw()
         self.running = False
-
     def game_over(self):
         pass
-
-
 g = Game()
 g.new()
 while g.running:
     g.main()
     g.game_over()
-
 pygame.quit()
 sys.exit()
--- Code/sprite.py
+++ Code/sprite.py
@@ -2,6 +2,7 @@ import pygame
 import math
 from config import *
 import random
+from icecream import ic 
 
 
 class Player(pygame.sprite.Sprite):
@@ -19,14 +20,20 @@ class Player(pygame.sprite.Sprite):
         self.x_change = 0
         self.y_change = 0
         self.facing = "down"  # movements of the character
-        self.image = pygame.Surface([self.width, self.height])
-        self.image.fill(RED)
+        image_to_load = pygame.image.load("img/player/player_look_down.png")
+        self.image = pygame.Surface([self.width,self.height])
+        self.image = self.image.convert_alpha()
+        self.image.set_colorkey(BLACK)
+        self.image.blit(image_to_load,(0,0))
+
 
         self.rect = self.image.get_rect()  # hitbox = image the same
         self.rect.x = self.x
         self.rect.y = self.y
+        self.weapon = Wepon(game,self)
 
     def update(self):
+        
         self.movement()
         key = pygame.key.get_pressed()
         self.rect.x += self.x_change
@@ -47,6 +54,7 @@ class Player(pygame.sprite.Sprite):
         self.x_change = 0 
         self.y_change = 0
         self.collide_powerup()
+        self.weapon.update()
         if self.collide_portal():
             return True
         return False 
@@ -94,7 +102,7 @@ class Player(pygame.sprite.Sprite):
 
         if direction == "x":
             # False ist, ob wir der Sprite löschen wollen
-            hits = pygame.sprite.spritecollide(self, self.game.blocks,
+            hits = pygame.sprite.spritecollide(self, self.game.waende,
                                                False)  # prüft ob die rect zweier Sprites miteinander kollidieren
             if hits:
                 if self.x_change > 0:
@@ -107,7 +115,7 @@ class Player(pygame.sprite.Sprite):
                     # for sprite in self.game.all_sprites:
                     #    sprite.rect.x -= PLAYER_SPEED 
         if direction == "y":
-            hits = pygame.sprite.spritecollide(self, self.game.blocks,
+            hits = pygame.sprite.spritecollide(self, self.game.waende,
                                                False)  # prüft obt die rect zweier Sprites miteinander kollidieren
             if hits:
                 if self.y_change > 0:
@@ -132,11 +140,12 @@ class Block(pygame.sprite.Sprite):
 
         self.height = BOXSIZE
 
-    def get_form_img(self,Path):
+    def get_form_img(self,Path,Alpha):
         image_to_load = pygame.image.load(Path)
         self.image = pygame.Surface([self.width,self.height])
         self.image.blit(image_to_load,(0,0))
-
+        if Alpha:
+            self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect_change_x = 0
         self.rect_change_y = 0
@@ -153,7 +162,7 @@ class Powerup(Block):
     def __init__(self, game, x, y):
         Block.__init__(self,game,x,y)
        
-        self.groups = self.game.all_sprites, self.game.powerup
+        self.groups = self.game.all_sprites, self.game.powerup,self.game.blocks
         pygame.sprite.Sprite.__init__(self, self.groups)
         Block.get_form_colour(self,BOOST)
         
@@ -165,21 +174,42 @@ class Wall(Block):
 
         Block.__init__(self,game,x,y)
         if transparent:
-            self.groups = self.game.all_sprites,self.game.blocks
+            self.groups = self.game.all_sprites,self.game.blocks, self.game.waende
         else:
-            self.groups = self.game.all_sprites
+            self.groups = self.game.all_sprites,self.game.blocks
         pygame.sprite.Sprite.__init__(self,self.groups)
         Block.get_form_colour(self,SAND)
 class Floor(Block):
     def __init__(self,game,x,y):
         Block.__init__(self,game,x,y)
         
-        self.groups = self.game.all_sprites
+        self.groups = self.game.all_sprites,self.game.blocks
         pygame.sprite.Sprite.__init__(self,self.groups)
-        Block.get_form_img(self,"img/floorbig.png")
+        Block.get_form_img(self,"img/floorbig.png",False)
 class Portal(Block):
     def __init__(self,game,x,y):
         Block.__init__(self,game,x,y)
-        self.groups = self.game.all_sprites,self.game.portal
+        self.groups = self.game.all_sprites,self.game.portal,self.game.blocks
         pygame.sprite.Sprite.__init__(self,self.groups)
-        Block.get_form_img(self,"img/portal.png")
+        Block.get_form_img(self,"img/portal.png",True)
+
+class Wepon(pygame.sprite.Sprite):
+    def __init__(self,game,Player):
+        self.player = Player
+        self.game = game 
+        self._layer = WEAPON_LAYER
+        self.x = Player.x
+        self.y = Player.y
+        self.width = 40
+
+        self.height = 10
+        self.groups = self.game.all_sprites, self.game.weapons
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.image = pygame.Surface([self.width,self.height])
+        self.image.fill((255,255,0))
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+    def update(self):
+        self.rect.x = 400
+        self.rect.y = 400
